78e3de643ebd6bdbabaddb4a37c791c6c38068e7
Tina Coleman <colemanserious@gmail.com>
2013-07-11 21:35:09 +0000

    Added grader code

--- grader.js
+++ grader.js
@@ -26,13 +26,14 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function (infile) {
 	var instr = infile.toString();
 	if (!fs.existsSync(instr)) {
-	  console.log("%s does not exist.  Exiiting", instr);
+	  console.log("%s does not exist.  Exiting", instr);
 	  process.exit(1);  //http://node.js.org/api/process.html#process_process_exit_code
 	}
 	return instr;
@@ -41,19 +42,49 @@ var assertFileExists = function (infile) {
 var cheerioHtmlFile = function(htmlfile) {
 	return cheerio.load(fs.readFileSync(htmlfile));
 }
+var cheerioHtmlStream = function(htmlString) {
+//	console.log("Parsing HTML String " + htmlString);
+	return cheerio.load(htmlString);
+}
 
 var loadChecks = function(checksfile) {
 	return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-	$ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, url) {
+	var $;
+	if (url) {
+		rest.get(url).on('complete', function(result) {
+			if (result instanceof Error) {
+				console.log("Error in getting url: " + result.message);
+				this.retry(50000);
+			} else {
+				//console.log(result);
+				$ = cheerioHtmlStream(result);
+				//console.log("Finished checking...: " + $);
+				//return checkChecks($, checksfile);
+				console.log(JSON.stringify(checkChecks($, checksfile), null, 4));
+			}
+		});
+	} else {
+		$ = cheerioHtmlFile(htmlfile);
+		//console.log("Finished checking...: " + $);
+		//return checkChecks($, checksfile);
+		console.log(JSON.stringify(checkChecks($, checksfile), null, 4));
+	}
+};
+
+var checkChecks = function( $, checksfile ) {
+
 	var checks = loadChecks(checksfile).sort();
+	//console.log("Loaded checks");
 	var out = {};
 	for (var ii in checks) {
 		var present = $(checks[ii]).length > 0;
+		//console.log("Found something: "  + ($(checks[ii]).length > 0));
 		out[checks[ii]] = present;
 	}
+	//console.log("Results: " + out);
 	return out;
 };
 
@@ -67,10 +98,11 @@ if (require.main == module) {
 	program
 	  .option('-c, --checks <check-file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	  .option('-f, --file <html-file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+  	  .option('u, --url <url>', 'URL to check')
 	  .parse(process.argv);
-  	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	//var checkJson = checkHtmlFile(program.file, program.checks, program.url);
+	var outJson = checkHtmlFile(program.file, program.checks, program.url);
+	//var outJson = JSON.stringify(checkJson, null, 4);
 } else {
 	exports.checkHtmlFile = checkHtmlFile;
 }
